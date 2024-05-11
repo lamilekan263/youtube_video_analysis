6eeedad775b314413866759f5a220bb646aa7e6d
lamilekan263 <lamilekan263@gmail.com>
2024-05-11 08:10:32 +0100

    feat: added single channel pointer

--- youtube_stat.py
+++ youtube_stat.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from tqdm import tqdm
 
 class YT_Stat:
     def __init__(self,api_key: str,channel_id:str):
@@ -23,8 +24,27 @@ class YT_Stat:
     
     def get_channel_video_data(self):
         channel_videos  = self._get_channel_videos(limit=50)
-        print(channel_videos)
+        print(len(channel_videos))
+        parts = ["snippet", "statistics","contentDetails"]
+        for video_id in tqdm(channel_videos):
+            for part in parts:
+                data = self._get_single_video_data(video_id,part)
+                channel_videos[video_id].update(data)
+        self.video_data = channel_videos
+        return channel_videos 
 
+    def _get_single_video_data(self,video_id,part):
+        url = f"https://www.googleapis.com/youtube/v3/videos?part={part}&id={video_id}&key={self.api_key}"
+        json_url = requests.get(url)
+        data = json.loads(json_url.text)
+        try:
+            data = data['items'][0][part]
+        except:
+            print('error')
+            data = dict()
+
+        return data
+    
     def _get_channel_videos(self, limit=None):
         url = f"https://www.googleapis.com/youtube/v3/search?key={self.api_key}&channelId={self.channel_id}&part=id&order=date"
         if limit is not None and isinstance(limit, int):
@@ -59,11 +79,18 @@ class YT_Stat:
         return channel_videos,next_page_token
 
     def dump(self):
-        if self.channel_statistics == None:
+        if self.channel_statistics is None or self.video_data is None:
+            print('data is None')
             return
-        channel_title = 'Python Engineer'
+        fused_data = {self.channel_id:
+                      {
+                    "channel_statistics":self.channel_statistics,
+                    "video_data": self.video_data
+                }
+            }
+        channel_title = self.video_data.popitem()[1].get('channelTitle',self.channel_id)
         channel_title = channel_title.replace(" ", '_').lower()
         file_name = channel_title +'.json'
         with open(file_name,'w') as f:
-            json.dump(self.channel_statistics,f, indent=4)
+            json.dump(fused_data,f, indent=4)
         print('file dumped')
